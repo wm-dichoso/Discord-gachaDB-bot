8d349d7e636c52ccb1d08d9b4091cc58d0618c90
Yuru Sia <yurusiaaa02@gmail.com>
2026-01-21 16:54:28 +0800

    completed some function on db manager. 2/3 done XD
    is this too slow pace ? XD

--- database_manager.py
+++ database_manager.py
@@ -119,7 +119,26 @@ class DatabaseManager:
         cur.execute("SELECT * FROM games WHERE game_id = ?", (game_id,))
 
         return cur.fetchone() is not None
+    
+    def game_exists_with_name(self, game_name):
+        if not self.is_connected():
+            print("Database is not connected")
+            return False
+        
+        cur = self.connection.cursor()
+        cur.execute("SELECT * FROM games WHERE game_name = ?", (game_name,))
 
+        return cur.fetchone() is not None
+
+    def game_exists_with_channel_ID(self, channel_id):        
+        if not self.is_connected():
+            print("Database is not connected")
+            return False
+        
+        cur = self.connection.cursor()
+        cur.execute("SELECT * FROM games WHERE game_name = ?", (channel_id,))
+
+        return cur.fetchone() is not None
 
     def banner_exists(self, banner_id):
         if not self.is_connected():
@@ -130,7 +149,27 @@ class DatabaseManager:
         cur.execute("SELECT * FROM banners WHERE banner_id = ?", (banner_id,))
 
         return cur.fetchone() is not None
+    
+    def banner_name_exists(self, game_id, banner_name):
+        if not self.is_connected():
+            print("Database is not connected")
+            return False
+        
+        cur = self.connection.cursor()
+        cur.execute("SELECT * FROM banners WHERE banner_name = ? AND game_id = ?", (banner_name, game_id,))
 
+        return cur.fetchone() is not None
+
+    def pull_entry_exists(self, pull_id):
+        if not self.is_connected():
+            print("Database is not connected")
+            return False
+        
+        cur = self.connection.cursor()
+        cur.execute("SELECT * FROM pull_history WHERE pull_id = ?",(pull_id,))
+
+        return cur.fetchone() is not None
+    
     def session_exists(self, session_id):
         if not self.is_connected():
             print("Database is not connected")
@@ -162,18 +201,17 @@ class DatabaseManager:
             print("banner does not exists")
             return
         
-        cur = self.connection.cursor()
-        cur.execute("SELECT current_pity FROM banners WHERE banner_id = ?", (banner_id,))
-        res = cur.fetchone()
-        print(f"current pity is {res[0]}")
+        with self.connection:        
+            cur = self.connection.cursor()
+            cur.execute("SELECT current_pity FROM banners WHERE banner_id = ?", (banner_id,))
+            res = cur.fetchone()
+            print(f"current pity is {res[0]}")
 
-        new_pity = int(res[0]) + pity
+            new_pity = int(res[0]) + pity
 
-        cur.execute("UPDATE banners SET current_pity = ? WHERE banner_id = ?", (new_pity, banner_id,))
-        print(f"pity updated from {res[0]} to {new_pity}")
-        self.connection.commit()
-
-        return cur.rowcount > 0
+            cur.execute("UPDATE banners SET current_pity = ? WHERE banner_id = ?", (new_pity, banner_id,))
+            print(f"pity updated from {res[0]} to {new_pity}")
+            return cur.rowcount > 0
 
     def reset_pity(self, banner_id, pity):
         if not self.is_connected():
@@ -184,16 +222,15 @@ class DatabaseManager:
             print("banner does not exists")
             return
         
-        cur = self.connection.cursor()
-        cur.execute("UPDATE banners SET current_pity = ? WHERE banner_id = ?", (pity, banner_id,))
-        self.connection.commit()
-
-        return cur.rowcount > 0
+        with self.connection:
+            cur = self.connection.cursor()
+            cur.execute("UPDATE banners SET current_pity = ? WHERE banner_id = ?", (pity, banner_id,))
+            return cur.rowcount > 0
  
 
     # crud operations for the tables apparently
 
-    # for the meta table !!
+    #region for the meta table !!
 
     def get_meta_version(self):
         if not self.is_connected():
@@ -210,69 +247,261 @@ class DatabaseManager:
             print("Database is not connected")
             return 
         
-        cur = self.connection.cursor()
-        cur.execute("INSERT INTO meta (version, created_at, last_modified) VALUES (?, CURRENT_TIMESTAMP, CURRENT_TIMESTAMP)", (version,))
-        self.connection.commit()
-        print(f"version {version} is added to the meta")
+        with self.connection:        
+            cur = self.connection.cursor()
+            cur.execute("INSERT INTO meta (version, created_at, last_modified) VALUES (?, CURRENT_TIMESTAMP, CURRENT_TIMESTAMP)", (version,))
+            print(f"version {version} is added to the meta")
 
     def update_version(self, version):
+        if not self.is_connected():
+            print("Database is not connected")
+            return False 
+        
+        cur = self.connection.cursor()
+        cur.execute("UPDATE meta SET last_modified = CURRENT_TIMESTAMP WHERE version = ?", (version,))
+        self.connection.commit()
+
+        return cur.rowcount > 0
+    
+    # endregion
+
+    #region for the games !!
+    # should be able to: add new games, list all the games on the table, get a particular game 
+    # and display some info, update a game info, delete the game from the table.
+    def add_games(self, game, ch_id):
+        if not self.is_connected():
+            print("Database is not connected")
+            return False 
+        
+        # check if game already exists 
+        if self.game_exists_with_name(game):
+            print(f"Game {game} already exists")
+            return False
+        
+        with self.connection:        
+            cur = self.connection.cursor()
+            cur.execute("INSERT INTO games (game_name, channel_id) VALUES (?, ?)", (game, ch_id,))
+            print(f"Game {game} is added to the games table")
+            return cur.rowcount > 0
+
+    def get_game_by_id(self, game_id):
+        if not self.is_connected():
+            print("Database is not connected")
+            return 
+        
+        if not self.game_exists():
+            print(f"game with game id: {game_id} does not exists.")
+            return
+        
+        cur = self.connection.cursor()
+        cur.execute("SELECT * FROM games WHERE game_id = ?",(game_id,))
+        return cur.fetchone()
+        
+    def list_games(self):
         if not self.is_connected():
             print("Database is not connected")
             return 
         
         cur = self.connection.cursor()
-        cur.execute("UPDATE meta SET last_modified = CURRENT_TIMESTAMP WHERE version = ?", (version,))
-        # UPDATE banners SET current_pity = ? WHERE banner_id = ?
-        self.connection.commit()
+        cur.execute("SELECT game_name FROM games")
+        return cur.fetchall()
 
-        return cur.rowcount > 0
+    def update_game_name(self, game_id, game_name):        
+        if not self.is_connected():
+            print("Database is not connected")
+            return False 
+        
+        if not self.game_exists():
+            print(f"game with game id: {game_id} does not exists.")
+            return False
+        
+        with self.connection:        
+            cur = self.connection.cursor()
+            cur.execute("UPDATE games SET game_name = ? WHERE game_id = ?", (game_name, game_id,))
+            return cur.rowcount > 0
 
-    # for the games !!
-    # should be able to: add new games, list all the games on the table, get a particular game 
-    # and display some info, update a game info, delete the game from the table.
-    def add_games(self):
-        pass
+    def update_game_channel(self, game_id, channel_id):
+        if not self.is_connected():
+            print("Database is not connected")
+            return False 
+        
+        if not self.game_exists():
+            print(f"game with game id: {game_id} does not exists.")
+            return False
+        
+        with self.connection:        
+            cur = self.connection.cursor()
+            cur.execute("UPDATE games SET channel_id = ? WHERE game_id = ?", (channel_id, game_id,))
+            return cur.rowcount > 0
 
-    def get_game(self):
-        pass
+    def delete_game(self, game_id):
+        if not self.is_connected():
+            print("Database is not connected")
+            return False 
+        
+        if not self.game_exists():
+            print(f"game with game id: {game_id} does not exists.")
+            return False
+        
+        with self.connection:        
+            cur = self.connection.cursor()
+            cur.execute("DELETE FROM games WHERE game_id = ?", (game_id,))
+            return cur.rowcount > 0
+        
+    # endregion
 
-    def list_games(self):
-        pass
-
-    def update_game(self):
-        pass
-
-    def delete_game(self):
-        pass
-
-    # for the banners !!
+    #region for the banners !!
     # add new banner/ banner name to the table, get a banner for a particular game,
     # update a banner's information.
-    def add_banners(self):
-        pass
 
-    def get_banner(self):
-        pass
+    def add_banner(self, game_id, banner_name, current_pity, max_pity):
+        if not self.is_connected():
+            print("Database is not connected")
+            return False 
+        
+        # check if banner already exists first 
+        if self.banner_name_exists(game_id, banner_name):
+            print(f"Banner: {banner_name} already exists on the game id: {game_id}")
+            return False
+        
+        with self.connection:        
+            cur = self.connection.cursor()
+            cur.execute("INSERT INTO banners (game_id, banner_name, current_pity, max_pity) VALUES (?, ?, ?, ?)", (game_id, banner_name,current_pity, max_pity,))
+            print(f"Banner: {banner_name} is added to db")
+            return cur.rowcount > 0
 
-    def get_game_banners(self):
-        pass
+    def get_banner(self, banner_id):
+        if not self.is_connected():
+            print("Database is not connected")
+            return 
+        
+        cur = self.connection.cursor()
+        cur.execute("SELECT * FROM banners WHERE banner_id = ?", (banner_id,))
+        res = cur.fetchone()
+        return res
 
-    def update_banner_info(self):
-        pass
+    def get_game_banners(self, game_id):
+        if not self.is_connected():
+            print("Database is not connected")
+            return False 
+        
+        if not self.game_exists(game_id):
+            print(f"Game id: {game_id} does not exists")
+            return False
+        
+        cur = self.connection.cursor()
+        cur.execute("SELECT banner_name, current_pity, last_updated FROM banners WHERE game_id = ?", (game_id,))
+        return cur.fetchall()
 
-    def delete_banner(self):
-        pass
+    def update_banner_name(self, banner_id, new_banner_name):
+        if not self.is_connected():
+            print("Database is not connected")
+            return False 
+        
+        if not self.banner_exists():
+            print(f"Banner with banner id: {banner_id} does not exists.")
+            return False
+        
+        with self.connection:        
+            cur = self.connection.cursor()
+            cur.execute("UPDATE banners SET banner_name = ? WHERE banner_id = ?", (new_banner_name, banner_id,))
+            return cur.rowcount > 0
+
+    def update_banner_pity(self, banner_id, new_pity):
+        if not self.is_connected():
+            print("Database is not connected")
+            return False 
+        
+        if not self.banner_exists():
+            print(f"Banner with banner id: {banner_id} does not exists.")
+            return False
+        
+        with self.connection:        
+            cur = self.connection.cursor()
+            cur.execute("UPDATE banners SET current_pity = ? WHERE banner_id = ?", (new_pity, banner_id,))
+            return cur.rowcount > 0
+    
+    def update_banner_max_pity(self, banner_id, new_max_pity):
+        if not self.is_connected():
+            print("Database is not connected")
+            return False 
+        
+        if not self.banner_exists():
+            print(f"Banner with banner id: {banner_id} does not exists.")
+            return False
+        
+        with self.connection:        
+            cur = self.connection.cursor()
+            cur.execute("UPDATE banners SET max_pity = ? WHERE banner_id = ?", (new_max_pity, banner_id,))
+            return cur.rowcount > 0
+
+    def delete_banner(self, banner_id):
+        if not self.is_connected():
+            print("Database is not connected")
+            return False 
+        
+        if not self.banner_exists(banner_id):
+            print(f"Cannot be deleted. Banner with banner id: {banner_id} does not exists.")
+            return False
+        
+        with self.connection:        
+            cur = self.connection.cursor()
+            cur.execute("DELETE FROM banners WHERE banner_id = ?", (banner_id,))
+            return cur.rowcount > 0
+        
+    # endregion
 
     # for the pull history !!
     # browse a list history of a particular banner, delete an entry
-    def add_pull(self):
-        pass
 
-    def get_pulls_by_banner(self):
-        pass
+    def add_pull(self, entry_name, banner_id, pity, notes):
+        if not self.is_connected():
+            print("Database is not connected")
+            return False 
+        
+        # check if banner for this pull actually xists 
+        if not self.banner_exists(banner_id):
+            print(f"Banner id: {banner_id} does not exists")
+            return False
+        
+        with self.connection:        
+            cur = self.connection.cursor()            
+            cur.execute("""
+                INSERT INTO pull_history (banner_id, game_id, entry_name, pity, notes)
+                SELECT
+                    b.banner_id, b.game_id, ?, ?, ?
+                FROM banners b
+                WHERE b.banner_id = ?
+            """, (entry_name, pity, notes, banner_id))
+            print(f"Added new unit")
+            return cur.rowcount > 0
 
-    def delete_pull(self):
-        pass
+    def get_pulls_by_banner(self, banner_id):
+        if not self.is_connected():
+            print("Database is not connected")
+            return False 
+        
+        if not self.banner_exists(banner_id):
+            print(f"banner id: {banner_id} does not exists")
+            return False
+        
+        cur = self.connection.cursor()
+        cur.execute("SELECT entry_name, pity, notes, timestamp FROM pull_history WHERE banner_id = ?", (banner_id,))
+        return cur.fetchall()
+        
+    def delete_pull(self, pull_id):
+        if not self.is_connected():
+            print("Database is not connected")
+            return False
+        
+        if not self.pull_entry_exists(pull_id):
+            print(f"Cannot be deleted. pull entry id: {pull_id} does not exist.")
+            return False
+        
+        with self.connection:        
+            cur = self.connection.cursor()
+            cur.execute("DELETE FROM pull_history WHERE pull_id = ?", (pull_id,))
+            return cur.rowcount > 0
 
     # for the session history !!
     # add new session, browse history of sessions, delete session
